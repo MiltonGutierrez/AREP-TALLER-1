3291e9e4884fcade70e93db15e4f1e92e7d0de02
MiltonGutierrez <milton.gutierrez-l@mail.escuelaing.edu.co>
2025-01-28 12:40:37 -0500

    Carga HTML, CSS, JS, e imagenes

--- src/main/java/edu/escuelaing/arep/taller1/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/taller1/HttpServer.java
@@ -1,6 +1,7 @@
 package edu.escuelaing.arep.taller1;
 import java.net.*;
 import java.io.*;
+import java.nio.file.Files;
 
 public class HttpServer {
 
@@ -23,82 +24,20 @@ public class HttpServer {
 
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            String inputLine, outputLine;
+            BufferedOutputStream dataOut = new BufferedOutputStream(clientSocket.getOutputStream());
+ 
+            String request = in.readLine();
+            String[] parts = request.split(" ");
+            String httpVerb = parts[0];
+            String resource = parts[1];
+            URI resourceURI = new URI(resource);
+            printRequestHeaders(in);
 
-            boolean isFirstLine = true;
-            String file = "";   
-            while ((inputLine = in.readLine()) != null) {
-                if(isFirstLine){
-                    file = inputLine.split(" ")[1];
-                    isFirstLine = false;
-                }
-                System.out.println("Received: " + inputLine);
-                if (!in.ready()) {
-                    break;
-                }
+            if(httpVerb.equals("GET")){
+                handleGetRequests(resource, resourceURI, out, dataOut);
             }
-
-            URI resourceURI = new URI(file);
-            System.out.println("URI: " + resourceURI);
-
-            if(resourceURI.getPath().startsWith("/app/hello")){
-                outputLine = helloRestService(resourceURI.getPath(), resourceURI.getQuery());
-                out.println(outputLine);
-            }
-            else{
-                outputLine = "HTTP/1.1 200 OK\r\n"
-                    + "Content-Type: text/html\r\n"
-                    + "\r\n"
-                    + "<!DOCTYPE html>\r\n" + //
-                    "<html>\r\n" + //
-                    "    <head>\r\n" + //
-                    "        <title>Form Example</title>\r\n" + //
-                    "        <meta charset=\"UTF-8\">\r\n" + //
-                    "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + //
-                    "    </head>\r\n" + //
-                    "    <body>\r\n" + //
-                    "        <h1>Form with GET</h1>\r\n" + //
-                    "        <form action=\"/hello\">\r\n" + //
-                    "            <label for=\"name\">Name:</label><br>\r\n" + //
-                    "            <input type=\"text\" id=\"name\" name=\"name\" value=\"John\"><br><br>\r\n" + //
-                    "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\r\n" + //
-                    "        </form> \r\n" + //
-                    "        <div id=\"getrespmsg\"></div>\r\n" + //
-                    "\r\n" + //
-                    "        <script>\r\n" + //
-                    "            function loadGetMsg() {\r\n" + //
-                    "                let nameVar = document.getElementById(\"name\").value;\r\n" + //
-                    "                const xhttp = new XMLHttpRequest();\r\n" + //
-                    "                xhttp.onload = function() {\r\n" + //
-                    "                    document.getElementById(\"getrespmsg\").innerHTML =\r\n" + //
-                    "                    this.responseText;\r\n" + //
-                    "                }\r\n" + //
-                    "                xhttp.open(\"GET\", \"/app/hello?name=\"+nameVar);\r\n" + //
-                    "                xhttp.send();\r\n" + //
-                    "            }\r\n" + //
-                    "        </script>\r\n" + //
-                    "\r\n" + //
-                    "        <h1>Form with POST</h1>\r\n" + //
-                    "        <form action=\"/app/hellopost\">\r\n" + //
-                    "            <label for=\"postname\">Name:</label><br>\r\n" + //
-                    "            <input type=\"text\" id=\"postname\" name=\"name\" value=\"John\"><br><br>\r\n" + //
-                    "            <input type=\"button\" value=\"Submit\" onclick=\"loadPostMsg(postname)\">\r\n" + //
-                    "        </form>\r\n" + //
-                    "        \r\n" + //
-                    "        <div id=\"postrespmsg\"></div>\r\n" + //
-                    "        \r\n" + //
-                    "        <script>\r\n" + //
-                    "            function loadPostMsg(name){\r\n" + //
-                    "                let url = \"/hellopost?name=\" + name.value;\r\n" + //
-                    "\r\n" + //
-                    "                fetch (url, {method: 'POST'})\r\n" + //
-                    "                    .then(x => x.text())\r\n" + //
-                    "                    .then(y => document.getElementById(\"postrespmsg\").innerHTML = y);\r\n" + //
-                    "            }\r\n" + //
-                    "        </script>\r\n" + //
-                    "    </body>\r\n" + //
-                    "</html>";
-                    out.println(outputLine);
+            else if(httpVerb.equals("POST")){
+                
             }
             out.close();
             in.close();
@@ -109,15 +48,67 @@ public class HttpServer {
 
     private static boolean running = true;
     public static void main(String[] args) throws IOException, URISyntaxException {
-        
+        HttpServer.runServer();
     }
 
-    private static String helloRestService(String path, String query){
-        query = query.substring(5);
-        String response = "HTTP/1.1 200 OK\r\n"
-                    + "Content-Type: application/json\r\n"
-                    + "\r\n"
-                    +  "{ \"name\": "+ "\""+ query +"\"" + "}";
-        return response;
+    private static void handleGetRequests(String requestedResource, URI resourceURI, PrintWriter out, BufferedOutputStream dataOut) throws IOException{
+        String query = resourceURI.getQuery();
+        String path = resourceURI.getPath();
+        String contentType = getContentType(requestedResource);
+        boolean isIndexPage = requestedResource.equals("/") && query == null && path.equals("/");
+        if(isIndexPage){
+            File htmlPage = new File(WEB_ROOT, "/notes.html");
+            int pageLength = (int) htmlPage.length();
+            out.println("HTTP/1.1 200 OK");
+            out.println("Content-Type: text/html");
+            out.println("Content-Length: " + pageLength);
+            out.println();
+            out.flush();
+            byte[] fileBytes = Files.readAllBytes(htmlPage.toPath());
+            dataOut.write(fileBytes);
+            dataOut.flush();
+        }
+        else{
+            File resource = new File(WEB_ROOT, requestedResource);
+            if(resource.exists()){
+                int resourceLength = (int) resource.length();
+                out.println("HTTP/1.1 200 OK");
+                out.println("Content-Type: " + contentType);
+                out.println("Content-Length: " + resourceLength);
+                out.println();
+                out.flush();
+                byte[] fileBytes = Files.readAllBytes(resource.toPath());
+                dataOut.write(fileBytes);
+                dataOut.flush();
+            }
+            else{
+                out.println("HTTP/1.1 404 Not Found");
+                out.println("Content-Type: text/html");
+                out.println();
+                out.println("<html><body><h1>404 Not Found</h1></body></html>");
+                out.flush();
+            }
+        }
+        
+    } 
+
+    private static String getContentType(String requestedResource){
+        if(requestedResource.endsWith(".html")) return "text/html";
+        if(requestedResource.endsWith(".css")) return "text/css";
+        if(requestedResource.endsWith(".js")) return "text/javascript";
+        if(requestedResource.endsWith(".png")) return "image/png";
+        if(requestedResource.endsWith(".jpg")) return "image/jpg";
+        if(requestedResource.endsWith(".jpeg")) return "image/jpeg";
+        return null;
+    }
+
+    private static void printRequestHeaders(BufferedReader in) throws IOException{
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println("Header: " + inputLine);
+            if (!in.ready()) {
+                break;
+            }
+        }
     }
 }
